2900d2babbc633df0a77accabb46debb8d507b21
miaoda <miaoda@baidu.com>
2026-04-19 12:10:17 +0700

    feat: add map PWA icon with provided image asset

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -30,12 +30,12 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "/favicon.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/favicon.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
